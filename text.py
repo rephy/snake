5f3650b5a7dc701784a2fbe6c7d8dfcbe6062220
Raphael Manayon <rephy@Raphaels-MacBook-Air.local>
2024-04-29 14:19:20 -0700

    Add game over message (not functionality)

--- text.py
+++ text.py
@@ -13,3 +13,11 @@ class Texts(Turtle):
     def keep_score(self, score):
         self.clear()
         self.write(f"Score: {score}", font=("Verdana", 20, "bold"), align="center")
+
+    def game_over(self):
+        self.speed(10000)
+        self.penup()
+        self.goto(0, 0)
+        self.pendown()
+        self.speed(3)
+        self.write("Game over!", font=("Verdana", 35, "bold"), align="center")
